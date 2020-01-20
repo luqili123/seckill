37f0fc37490386d1912e7099cbc8f8d40e26b114
lql <1036110216@qq.com>
2020-01-20 14:34:29 +0800

    获取秒杀商品列表

--- src/main/java/com/edu/nju/seckill/controller/SeckillGoodsController.java
+++ src/main/java/com/edu/nju/seckill/controller/SeckillGoodsController.java
@@ -1,9 +1,13 @@
 package com.edu.nju.seckill.controller;
 
+import com.edu.nju.seckill.common.CommonResult;
 import com.edu.nju.seckill.service.SeckillGoodsService;
 import io.swagger.annotations.Api;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author lql
@@ -15,4 +19,14 @@ public class SeckillGoodsController {
 
     @Autowired
     private SeckillGoodsService seckillGoodsService;
+
+//    @GetMapping("/seckill/slide")
+//    public CommonResult<?>  getSecGoodsItems(){
+//        return null;
+//    }
+
+//    @PostMapping("/seckill")
+//    public CommonResult<?> addSeckillGoods(){
+//
+//    }
 }
