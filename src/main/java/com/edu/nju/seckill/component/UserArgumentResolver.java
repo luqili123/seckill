edcdb2754441c6a0fbe4f222e48f1bf28e768b66
oltreamre9 <1134625169@qq.com>
2020-01-23 02:39:18 +0800

    参数解析完成 controller中直接使用user对象即可

---
+++ src/main/java/com/edu/nju/seckill/component/UserArgumentResolver.java
@@ -0,0 +1,72 @@
+package com.edu.nju.seckill.component;
+
+import com.edu.nju.seckill.common.CommonResult;
+import com.edu.nju.seckill.domain.User;
+import com.edu.nju.seckill.exception.TokenException;
+import com.edu.nju.seckill.utils.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 用户参数解析器 从head中通过token解析出User信息
+ */
+public class UserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    /***
+     * 请求头中包含token的标签名
+     */
+    private static final String Authorization="Authorization";
+    /***
+     * token以什么开头
+     */
+    private static final String StartWith="Bearer ";
+
+    @Autowired
+    private RedisUtil redisUtil;
+    /**
+     * 判断是否进行参数解析
+     * @param methodParameter
+     * @return
+     */
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return true;
+    }
+
+    /**
+     * 具体通过head进行user参数解析
+     * @param methodParameter
+     * @param modelAndViewContainer
+     * @param nativeWebRequest
+     * @param webDataBinderFactory
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter,
+                                  ModelAndViewContainer modelAndViewContainer,
+                                  NativeWebRequest nativeWebRequest,
+                                  WebDataBinderFactory webDataBinderFactory) throws Exception {
+        User user=null;
+        String header=nativeWebRequest.getHeader(Authorization);
+        if(header!=null&&!"".equals(header)){
+            if(header.startsWith(StartWith)) {
+                String token = header.substring(7);
+                user = redisUtil.getUser(token);
+            }
+        }
+        if(null==user){
+            user=new User();
+            user.setName("whn");
+//            throw new TokenException(CommonResult.unauthorized("token错误没有获取用户信息"));
+        }
+        return user;
+    }
+}
--- src/main/java/com/edu/nju/seckill/controller/FavoriteController.java
+++ src/main/java/com/edu/nju/seckill/controller/FavoriteController.java
@@ -1,18 +1,37 @@
 package com.edu.nju.seckill.controller;
 
+import com.edu.nju.seckill.common.CommonResult;
+import com.edu.nju.seckill.domain.User;
+import com.edu.nju.seckill.domain.dto.CarouselItems;
 import com.edu.nju.seckill.service.FavoriteService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author lql
  * @date 2020/1/11 20:25
  */
-@Controller
+@RestController
 @Api(tags = "收藏夹控制类")
 public class FavoriteController {
 
     @Autowired
     private FavoriteService favoriteService;
+
+    @ApiOperation(value = "get参数解析器测试",notes = "testGet")
+    @GetMapping("/favorite/get")
+    public String getTest(User user,  @RequestParam String string){
+        return user.getName()+string;
+    }
+
+    @ApiOperation(value = "post参数解析器测试",notes = "testPost")
+    @PostMapping("/favorite/post")
+    public String postTest(User user,  @RequestParam String string){
+        return user.getName()+string;
+    }
 }
