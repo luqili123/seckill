f94c6f143682de8f6757014824014bcdfefe46c4
lql <1036110216@qq.com>
2020-02-11 21:13:09 +0800

    跨域问题2

--- src/main/java/com/edu/nju/seckill/component/CorsFilter.java
+++ src/main/java/com/edu/nju/seckill/component/CorsFilter.java
@@ -26,7 +26,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Authorization, Accept");
         chain.doFilter(req, res);
     }
 }
