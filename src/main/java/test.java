b57a0d03a7018acb8b64059f9dc6cdae1d2c2910
lql <1036110216@qq.com>
2020-02-12 20:12:49 +0800

    导航栏按sort排序

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * @author lql
+ * @date 2020/2/12 15:13
+ */
+public class test {
+    public static void main(String[] args) {
+        String s1="a";
+        String s2=new String("b");
+        System.out.println(s2=="b");
+    }
+}
